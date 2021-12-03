500650f6ac1e08487ba269894f6831d7e15e0d8a
Sam Paxton <samuel.m.paxton@gmail.com>
2021-12-03 17:11:21 -0500

    day 2 part 2 complete

--- src/day2.java
+++ src/day2.java
@@ -18,9 +18,29 @@ public class day2 {
         return horizontalLocation * depth;
     }
 
+    public static int part2(List<MovementCommand> commands) {
+        int horizontalLocation = 0;
+        int depth = 0;
+        int aim = 0;
+
+        for (MovementCommand command : commands) {
+            if (command.direction.equals("forward")) {
+                horizontalLocation += command.amount;
+                depth += (aim * command.amount);
+            } else if (command.direction.equals("down")) {
+                aim += command.amount;
+            } else if (command.direction.equals("up")) {
+                aim -= command.amount;
+            }
+        }
+
+        return horizontalLocation * depth;
+    }
+
     public static void main(String[] args) {
         List<MovementCommand> commands = utils.readListOfMovementCommands("inputs/day2.txt");
 
-        System.out.println("Final horizontal position by final depth: " + part1(commands));
+        System.out.println("Final horizontal position by final depth (Part 1): " + part1(commands));
+        System.out.println("Final horizontal position by final depth (Part 2): " + part2(commands));
     }
 }
