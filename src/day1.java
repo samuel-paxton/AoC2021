b46265baaf3cf9e6af40b523bf9ca72b8511dfb7
Sam Paxton <samuel.m.paxton@gmail.com>
2021-12-03 16:53:17 -0500

    finish day 1 part 2

--- src/day1.java
+++ src/day1.java
@@ -2,10 +2,11 @@ import java.util.List;
 
 public class day1 {
     public static int part1(List<Integer> depths) {
-        int previousDepth = Integer.MAX_VALUE;
+        int previousDepth = depths.get(0);
         int numDepthIncreases = 0;
 
-        for (int currentDepth : depths) {
+        for (int i = 1; i < depths.size(); i++) {
+            int currentDepth = depths.get(i);
             if (currentDepth > previousDepth) {
                 numDepthIncreases++;
             }
@@ -16,14 +17,34 @@ public class day1 {
         return numDepthIncreases;
     }
 
-    public static int part2(List<Integer> depths) {
-        return 10;
+    public static int part2(List<Integer> depths, int slidingWindow) {
+        int previousWindowSum = 0;
+
+        for (int i = 0; i < slidingWindow; i++) {
+            previousWindowSum += depths.get(i);
+        }
+
+        int numSumIncreases = 0;
+        for (int i = 1; i <= depths.size() - slidingWindow; i++) {
+            int currentWindowSum = 0;
+            for (int j = i; j < i + slidingWindow; j++) {
+                currentWindowSum += depths.get(j);
+            }
+
+            if (currentWindowSum > previousWindowSum) {
+                numSumIncreases++;
+            }
+
+            previousWindowSum = currentWindowSum;
+        }
+
+        return numSumIncreases;
     }
 
     public static void main(String[] args) {
         List<Integer> depths = utils.readListOfNumbersFromFile("inputs/day1.txt");
 
         System.out.println("Number of times depth increased: " + part1(depths));
-        System.out.println("Number of times three-measurement sliding window increased: " + part2(depths));
+        System.out.println("Number of times three-measurement sliding window increased: " + part2(depths, 3));
     }
 }
